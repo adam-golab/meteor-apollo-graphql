28e953e20f37c52c9617685e73c6475153d1dbaa
Adam Golab <adam@brainhub.pl>
2018-02-07 20:47:45 +0100

    added resolver for resolutions

---
+++ imports/startup/server/register-api.js
@@ -0,0 +1,36 @@
+import { createApolloServer } from 'meteor/apollo';
+import { makeExecutableSchema } from 'graphql-tools';
+
+import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql';
+
+const testSchema = `
+type Query {
+  hi: String
+  resolutions: [Resolution]
+}
+`;
+
+const typeDefs = [
+  testSchema,
+  ResolutionsSchema,
+];
+
+const resolvers = {
+  Query: {
+    hi() {
+      return 'Hello';
+    },
+    resolutions() {
+      return [
+        {
+          _id: 'asd',
+          name: 'Get stuff done'
+        }
+      ]
+    }
+  }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+createApolloServer({ schema });
